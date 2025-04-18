c946f52583ae6e36e1d5c633aebf6225a41f92a6
Batu <164036859+batuhanbaydr@users.noreply.github.com>
2025-04-18 04:48:16 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:sonique/routes/login.dart';
 import 'package:sonique/routes/signup.dart';
 import 'package:sonique/routes/home.dart';
 import 'package:sonique/routes/rate.dart';
+import 'package:sonique/routes/artist_page.dart';
+import 'package:sonique/routes/search.dart';
 
 void main() {
   runApp(
@@ -16,6 +18,8 @@ void main() {
         '/login': (context) => Login(),
         '/home': (context) => Home(),
         '/rate': (context) => Rate(),
+        '/artist': (context) => ArtistPage(),
+        '/search': (context) => Search(),
       },
     ),
   );
