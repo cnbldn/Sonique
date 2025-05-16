96185269015c9df0d17b21ef5264ef90e0642180
aras <aras.samuk@sabanciuniv.edu>
2025-05-16 22:47:18 +0300

    signup and login commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:sonique/navigator.dart';
 import 'package:sonique/routes/welcome.dart';
 import 'package:sonique/routes/login.dart';
 import 'package:sonique/routes/signup.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:sonique/routes/artist_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    MaterialApp(
+  await Firebase.initializeApp();
+  
+  runApp(const SoniqueApp());
+}
+
+class SoniqueApp extends StatelessWidget{
+  const SoniqueApp ({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
       theme: ThemeData(fontFamily: 'Inter'),
       title: 'Sonique',
       routes: {
@@ -18,8 +26,10 @@ void main() async {
         '/signup': (context) => Signup(),
         '/login': (context) => Login(),
         '/mainNavigator': (context) => MainNavigator(),
+        "/artist": (context) => ArtistPage(),
       },
       initialRoute: '/',
-    ),
-  );
+    );
+  }
 }
+
